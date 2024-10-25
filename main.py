d989b14c6139d5b1428016f56dba5cc8c40a961e
251331 <ashleyarumyc@gmail.com>
2024-10-25 10:35:35 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from classWindow import Window
+
+ventana = Window()
+ventana.setDimensions(500,500)
+ventana.settitulo("mi primer")
+ventana.setResize(True)
+
+#print (ventana.getDimensions())
+#print(ventana.getResize())
+#print(ventana.gettitulo())
+
+ventana.crear_ventana()
